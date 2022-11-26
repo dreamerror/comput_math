bd2cbe7ee42c67b8f5c92e65af1f3606bc24eee0
dreamerror <66074257+dreamerror@users.noreply.github.com>
2022-11-26 20:51:48 +1000

    Monte Carlo method

--- utils/integrate.py
+++ utils/integrate.py
@@ -1,3 +1,5 @@
+from random import uniform
+
 import numpy as np
 from sympy import Expr, symbols
 
@@ -13,38 +15,40 @@ class Integrate:
 
     def left_rectangles(self, n: int):
         res = 0
-        x_vals = list(np.linspace(self.x_0, self.x_n, n))
-        step = x_vals[1] - x_vals[0]
-        x_vals.append(x_vals[::-1]+step)
+        x_vals = list(np.linspace(self.x_0, self.x_n, n+1))
         for i in range(n):
             res += self.__func(x_vals[i]) * (x_vals[i + 1] - x_vals[i])
         return res
 
     def right_rectangles(self, n: int):
         res = 0
-        x_vals = list(np.linspace(self.x_0, self.x_n, n))
-        step = x_vals[1] - x_vals[0]
-        x_vals.append(x_vals[::-1]+step)
+        x_vals = list(np.linspace(self.x_0, self.x_n, n+1))
         for i in range(1, n+1):
             res += self.__func(x_vals[i]) * (x_vals[i] - x_vals[i-1])
         return res
 
     def central_rectangles(self, n: int):
         res = 0
-        x_vals = list(np.linspace(self.x_0, self.x_n, n))
-        step = x_vals[1] - x_vals[0]
-        x_vals.append(x_vals[::-1]+step)
+        x_vals = list(np.linspace(self.x_0, self.x_n, n+1))
         for i in range(n):
             res += self.__func((x_vals[i] + x_vals[i+1])/2) * (x_vals[i+1] - x_vals[i])
         return res
 
     def cotes_method(self, n: int):
-        x_vals = list(np.linspace(self.x_0, self.x_n, n))
+        x_vals = list(np.linspace(self.x_0, self.x_n, n+1))
         step = x_vals[1] - x_vals[0]
-        x_vals.append(x_vals[::-1] + step)
         res = (self.__func(x_vals[0]) + self.__func(x_vals[n])) / 2
         for i in range(1, n):
             res += self.__func(x_vals[i])
         return res * step
 
+    def simpson(self):
+        return ((self.x_n - self.x_0) / 6) * (self.__func(self.x_0) + 4 * self.__func((self.x_0 + self.x_n) / 2) +
+                                              self.__func(self.x_n))
 
+    def monte_carlo(self, n: int):
+        res = 0
+        for i in range(n):
+            a = uniform(self.x_0, self.x_n)
+            res += self.__func(a)
+        return res * ((self.x_n - self.x_0) / n)
